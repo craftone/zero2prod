63a1c64d19735e7520b65384f055920e7c5021f9
Yohei Konno <konnoy@craftone.jpn.org>
2024-05-01 12:56:06 +0900

    Implements 5.3.6

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,13 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
-        PgPool::connect(configuration.database.connection_string().expose_secret())
-            .await
+        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{port}", port = configuration.application_port);
+    let address = format!(
+        "{host}:{port}",
+        host = configuration.application.host,
+        port = configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await
 }
