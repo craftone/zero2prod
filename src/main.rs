712e675f8154bbb880715cc626bdc416cb4fcd0d
Yohei Konno <konnoy@craftone.jpn.org>
2024-05-06 09:36:38 +0900

    Implement 6.6

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::net::TcpListener;
 
 use sqlx::postgres::PgPoolOptions;
-use sqlx::PgPool;
 use zero2prod::configurations::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
